ef331ecf1d5aa8621aacfad763bd7567a8721c6c
sponnet <stefaan@ponnet.com>
2022-12-05 17:57:03 +0100

    fix script typo

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,8 +5,7 @@ node /launchcmd/index.js
 sleep 10
 
 # and load the correct version it if needed - if this has already been done, this command does not do anything
-docker run --rm --privileged  --net=host --pid=host --ipc=host --volume /:/host  busybox  chroot /host /bin/bash -c "docker-compose -f /usr/src/dappnode/DNCORE/docker-compo
-se-dappmanager.yml up -d"
+docker run --rm --privileged  --net=host --pid=host --ipc=host --volume /:/host  busybox  chroot /host /bin/bash -c "docker-compose -f /usr/src/dappnode/DNCORE/docker-compose-dappmanager.yml up -d"
 
 # start admin UI
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
