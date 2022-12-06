135037d58915c78d74772aa94af23314950f362d
sponnet <stefaan@ponnet.com>
2022-12-06 09:32:01 +0100

    start nginx first - then run startup script

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# start admin UI
+nginx -c /etc/nginx/nginx.conf
+
+# wait for things to settle
+sleep 10
+
 # update the dappmanager if needed
 node /launchcmd/index.js
 sleep 10
@@ -7,7 +13,7 @@ sleep 10
 # and load the correct version it if needed - if this has already been done, this command does not do anything
 docker run --rm --privileged  --net=host --pid=host --ipc=host --volume /:/host  busybox  chroot /host /bin/bash -c "docker-compose -f /usr/src/dappnode/DNCORE/docker-compose-dappmanager.yml up -d"
 
-# start admin UI
-nginx -c /etc/nginx/nginx.conf -g "daemon off;"
+sleep infinity
+
 
 
