39b29fa934c3b4671d6e89f15c9d8a1242a3d55c
sponnet <stefaan@ponnet.com>
2023-01-19 19:46:28 +0100

    restart BIND if needed

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -13,6 +13,9 @@ sleep 10
 # and load the correct version it if needed - if this has already been done, this command does not do anything
 docker run --rm --privileged  --net=host --pid=host --ipc=host --volume /:/host  busybox  chroot /host /bin/bash -c "docker-compose -f /usr/src/dappnode/DNCORE/docker-compose-dappmanager.yml up -d"
 
+# restart bind if not running
+docker run --rm --privileged  --net=host --pid=host --ipc=host --volume /:/host  busybox  chroot /host /bin/bash -c "docker-compose -f /usr/src/dappnode/DNCORE/docker-compose-bind.yml up -d"
+
 sleep infinity
 
 
